6c26264cb3f5faeb313b01fdb69f5ee7e1bb35fa
pengxt <pengxt@funshion.com>
2012-12-14 16:32:37 +0800

    first init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * GitList 0.3
+ * https://github.com/klaussilveira/gitlist
+ */
+
+require 'vendor/autoload.php';
+$app = require 'boot.php';
+$app->run();
---
+++ src/GitList/Controller/CommitController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace GitList\Controller;
+
+use Silex\Application;
+use Silex\ControllerProviderInterface;
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class CommitController implements ControllerProviderInterface
+{
+    public function connect(Application $app)
+    {
+        $route = $app['controllers_factory'];
+
+        $route->get('{repo}/commits/{branch}/{file}', function($repo, $branch, $file) use ($app) {
+            $repository = $app['git']->getRepository($app['git.repos'] . $repo);
+            $type = $file ? "$branch -- $file" : $branch;
+            $pager = $app['util.view']->getPager($app['request']->get('page'), $repository->getTotalCommits($type));
+            $commits = $repository->getPaginatedCommits($type, $pager['current']);
+
+            foreach ($commits as $commit) {
+                $date = $commit->getDate();
+                $date = $date->format('m/d/Y');
+                $categorized[$date][] = $commit;
+            }
+
+            $template = $app['request']->isXmlHttpRequest() ? 'commits_list.twig' : 'commits.twig';
+
+            return $app['twig']->render($template, array(
+                'pager'          => $pager,
+                'repo'           => $repo,
+                'branch'         => $branch,
+                'branches'       => $repository->getBranches(),
+                'tags'           => $repository->getTags(),
+                'commits'        => $categorized,
+                'file'           => $file,
+            ));
+        })->assert('repo', '[\w-._]+')
+          ->assert('branch', '[\w-._]+')
+          ->assert('file', '.+')
+          ->value('branch', 'master')
+          ->value('file', '')
+          ->bind('commits');
+
+        $route->post('{repo}/commits/search', function(Request $request, $repo) use ($app) {
+            $repository = $app['git']->getRepository($app['git.repos'] . $repo);
+            $commits = $repository->searchCommitLog($request->get('query'));
+
+            foreach ($commits as $commit) {
+                $date = $commit->getDate();
+                $date = $date->format('m/d/Y');
+                $categorized[$date][] = $commit;
+            }
+
+            return $app['twig']->render('searchcommits.twig', array(
+                'repo'           => $repo,
+                'branch'         => 'master',
+                'file'           => '',
+                'commits'        => $categorized,
+                'branches'       => $repository->getBranches(),
+                'tags'           => $repository->getTags(),
+            ));
+        })->assert('repo', '[\w-._]+')
+          ->bind('searchcommits');
+
+        $route->get('{repo}/commit/{commit}/', function($repo, $commit) use ($app) {
+            $repository = $app['git']->getRepository($app['git.repos'] . $repo);
+            $commit = $repository->getCommit($commit);
+
+            return $app['twig']->render('commit.twig', array(
+                'branch'         => 'master',
+                'repo'           => $repo,
+                'commit'         => $commit,
+            ));
+        })->assert('repo', '[\w-._]+')
+          ->assert('commit', '[a-f0-9^]+')
+          ->bind('commit');
+
+        $route->get('{repo}/blame/{branch}/{file}', function($repo, $branch, $file) use ($app) {
+            $repository = $app['git']->getRepository($app['git.repos'] . $repo);
+            $blames = $repository->getBlame("$branch -- $file");
+
+            return $app['twig']->render('blame.twig', array(
+                'file'           => $file,
+                'repo'           => $repo,
+                'branch'         => $branch,
+                'branches'       => $repository->getBranches(),
+                'tags'           => $repository->getTags(),
+                'blames'         => $blames,
+            ));
+        })->assert('repo', '[\w-._]+')
+          ->assert('file', '.+')
+          ->assert('branch', '[\w-._]+')
+          ->bind('blame');
+
+        return $route;
+    }
+}
